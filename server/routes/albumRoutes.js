2b34923704af4279bc6b83fc4a00ed87a0f37012
TZ <tz116917@gmail.com>
2021-12-02 14:54:01 -0800

    basic albumRoutes

--- server/routes/albumRoutes.js
+++ server/routes/albumRoutes.js
@@ -4,27 +4,37 @@ const {Album, User} = require('../models');
 // Create new album
 router.post('/', async (req, res)=>{
     try{
-        const newAlbum = await Album.create(req.body);
-        res.status(200).json(newAlbum);
+        const data = await Album.create(req.body);
+        res.status(200).json(data);
     } catch(err){
         res.status(400).json(err)
     }
 })
 
-// Get all albums of a user by user id
-router.get("/:user_id", async (req, res) => {
+// Get album by album id
+router.get("/:id", async (req, res) => {
     try {
-        const data = await User.find({_id: mongojs.ObjectID(req.params.id)}).populate('album');
+        const data = await Album.find({_id: mongojs.ObjectID(req.params.id)});
         res.status(200).json(data);
     } catch (err){
         res.status(400).json(err);
     }
 })
 
-router.post('/', async (req, res)=>{
+// Get all albums of a user by user id
+router.get("/:user_id", async (req, res) => {
+    try {
+        const data = await User.find({_id: mongojs.ObjectID(req.params.user_id)}).populate('album');
+        res.status(200).json(data);
+    } catch (err){
+        res.status(400).json(err);
+    }
+})
+// Delete album by album id
+router.delete('/:id', async (req, res)=>{
     try{
-        const newAlbum = await Album.create(req.body);
-        res.status(200).json(newAlbum);
+        const data = await Album.remove(req.params.id);
+        res.status(200).json(data);
     } catch(err){
         res.status(400).json(err)
     }
