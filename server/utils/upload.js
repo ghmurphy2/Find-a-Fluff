687e9d70d3b4d755e5d7bf962eed2c96af647f11
Gavin Murphy <gavinhmurphy@gmail.com>
2021-12-02 14:22:40 -0800

    upload and div render, client id added for anonymous upload for url

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -5,14 +5,36 @@
 
 function report(vars, showType = false){
     if (showType ===true) console.log( vars);
+    // default no display, append on img display true
 }
 
 function addImg(e, content){
     var imageDiv = document.querySelector(e);
-    var NewImageDiv = document.createElement('div');
+    var newImageDiv = document.createElement('div');
+    // append image with new child
     newContent.innerHTML = content;
 
     while (newContent.firstChild){
-        imageDiv.appendChild
+        imageDiv.appendChild(newImageDiv)
     }
 }
+
+var feedback = function(res){
+    report(res, true);
+    if (res.success === true){
+        var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
+        // bootstrap required
+        document.querySelector('.status').classList.add('bg-info');
+        var content = 
+        'Image : ' + '<br><input class="image-url" value=\"' + get_link + '\"/>' 
+             + '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
+             addImg('.status', content);
+    }
+};
+
+new Imgur({
+    // anonymous upload, no auth needed
+    clientid: "07570dccac51516",
+    callback: feedback
+
+})
