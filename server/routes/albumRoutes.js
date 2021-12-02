8814ec45ef0b4fb2e7e12f9177f9a422d37d5af7
TZ <tz116917@gmail.com>
2021-12-02 14:47:44 -0800

    updated models, fixed associations

--- server/routes/albumRoutes.js
+++ server/routes/albumRoutes.js
@@ -1,11 +1,34 @@
+const router = require('express').Router();
+const {Album, User} = require('../models');
 
-const {Album} = require('../models');
-
-module.exports = {
-    async createAlbum(req, res){
+// Create new album
+router.post('/', async (req, res)=>{
+    try{
         const newAlbum = await Album.create(req.body);
-        res.status(200).json(new)
+        res.status(200).json(newAlbum);
+    } catch(err){
+        res.status(400).json(err)
     }
+})
+
+// Get all albums of a user by user id
+router.get("/:user_id", async (req, res) => {
+    try {
+        const data = await User.find({_id: mongojs.ObjectID(req.params.id)}).populate('album');
+        res.status(200).json(data);
+    } catch (err){
+        res.status(400).json(err);
+    }
+})
+
+router.post('/', async (req, res)=>{
+    try{
+        const newAlbum = await Album.create(req.body);
+        res.status(200).json(newAlbum);
+    } catch(err){
+        res.status(400).json(err)
+    }
+})
 
 
 
@@ -32,7 +55,4 @@ module.exports = {
 
 
 
-
-
-
-}
+module.exports = router
