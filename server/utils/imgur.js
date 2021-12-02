f8e87cf6f9f4830f95493b80d43d79405a7a2f75
Gavin Murphy <gavinhmurphy@gmail.com>
2021-12-02 14:10:49 -0800

    psuedo and report addimg function

---
+++ server/utils/imgur.js
@@ -0,0 +1,3 @@
+// format for imgur upload
+// drag or image address upload
+// post route for image, create ele, 
---
+++ server/utils/upload.js
@@ -0,0 +1,18 @@
+// create function for reporting image information
+// function for addimg
+// check for link throw back and display to div
+// new imgur api call, include client id for anonymous hosting
+
+function report(vars, showType = false){
+    if (showType ===true) console.log( vars);
+}
+
+function addImg(e, content){
+    var imageDiv = document.querySelector(e);
+    var NewImageDiv = document.createElement('div');
+    newContent.innerHTML = content;
+
+    while (newContent.firstChild){
+        imageDiv.appendChild
+    }
+}
