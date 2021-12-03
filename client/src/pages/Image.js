3cf89d2347189f9d1c5a59e10e4c0ed82ca0c698
Matt Weichel <64658667+maweiche@users.noreply.github.com>
2021-12-03 10:57:24 -0800

    Update Image.js

--- client/src/pages/Image.js
+++ client/src/pages/Image.js
@@ -1,4 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { Container, Col, Form, Button, Card } from 'react-bootstrap';
 
 // import Auth from '../utils/auth';
