8dba6597796349ad6ff2f9c1eb1e8bb26684f6ce
Matt Weichel <maweiche@gmail.com>
2021-12-05 15:31:19 -0800

    updated search breeds and activated localstorage

--- client/src/pages/SearchBreeds.js
+++ client/src/pages/SearchBreeds.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Col, Form, Button, Card, CardGroup, Row } from 'react-bootstrap';
 
-// import Auth from '../utils/auth';
-import {  searchDogBreeds } from '../utils/API';
-// import { saveBreedIds, getSavedBreedIds } from '../utils/localStorage';
+import Auth from '../utils/auth';
+import {  saveBreed, searchDogBreeds } from '../utils/API';
+import { saveBreedIds, getSavedBreedIds } from '../utils/localStorage';
 
 function SearchBreeds() {
     
@@ -13,12 +13,12 @@ function SearchBreeds() {
     const [searchInput, setSearchInput] = useState('');
 
     //create state to hold saved breedId values
-    // const [savedBreedIds, setSavedBreedIds] = useState(getSavedBreedIds());
+    const [savedBreedIds, setSavedBreedIds] = useState(getSavedBreedIds());
 
     //set up useEffect hook to save `savedBreedIds` list to localStorage on component unmount
-    // useEffect(() => {
-    //     return () => saveBreedIds(savedBreedIds);
-    // });
+    useEffect(() => {
+        return () => saveBreedIds(savedBreedIds);
+    });
 
     //create method to search for breeds and set state on form submit
     const handleFormSubmit = async (event) => {
@@ -50,30 +50,30 @@ function SearchBreeds() {
     };
 
     //create function to handle saving a breed to our database
-    // const handleSaveBreed = async (breedId) => {
-    //     //find breed in `searchedBreeds` state by matching id
-    //     const breedToSave = searchedBreeds.find((breed) => breed.breedId === breedId );
+    const handleSaveBreed = async (breedId) => {
+        //find breed in `searchedBreeds` state by matching id
+        const breedToSave = searchedBreeds.find((breed) => breed.breedId === breedId );
 
-    //     //get token
-    //     // const token = Auth.loggedIn() ? Auth.getToken() : null;
+        //get token
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    //     // if (!token) {
-    //     //     return false;
-    //     // }
+        if (!token) {
+            return false;
+        }
 
-    //     // try {
-    //     //     const response = await saveBreed(breedToSave, token);
+        try {
+            const response = await saveBreed(breedToSave, token);
 
-    //     //     if(!response.ok) {
-    //     //         throw new Error('ruff-row! something went wrong!');
-    //     //     }
+            if(!response.ok) {
+                throw new Error('ruff-row! something went wrong!');
+            }
 
-    //     //     //if breed successfully saves to user's account, save breed id to state
-    //     //     setSavedBreedIds([...savedBreedIds, breedToSave.breedId]);
-    //     // } catch (err) {
-    //     //     console.error(err);
-    //     }
-    // };
+            //if breed successfully saves to user's account, save breed id to state
+            setSavedBreedIds([...savedBreedIds, breedToSave.breedId]);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
     return (
         <div className="mainContainer">
@@ -118,7 +118,7 @@ function SearchBreeds() {
                                 ): null}
                                 <Card.Body>
                                     <Card.Title className="mb-2 text-dark">Check Out This Pup</Card.Title> 
-                                    {/* {Auth.loggedIn() && (
+                                    {Auth.loggedIn() && (
                                         <Button
                                         disabled={savedBreedIds?.some((savedBreedId) => savedBreedId === breed.breedId)}
                                         className='btn-block btn-info'
@@ -127,7 +127,7 @@ function SearchBreeds() {
                                             ? 'This breed has already been saved!'
                                             : 'Save this Breed!'}
                                         </Button> 
-                                    )}*/}
+                                    )}
                                 </Card.Body>
                             </Card>
                             </Col>
