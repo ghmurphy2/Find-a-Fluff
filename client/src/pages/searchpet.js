17c0150fc0b99eda1e8d94799bc00494ae8e7e2f
Gavin Murphy <gavinhmurphy@gmail.com>
2021-12-05 13:48:53 -0800

    added formik package, form building

--- client/src/pages/searchpet.js
+++ client/src/pages/searchpet.js
@@ -1,6 +1,7 @@
 // ================THIS PAGE DISPLAYS USER'S SAVED ALBUMS, SHOULD REDIRECT HERE AFTER LOGGING IN=========
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { Formik, Field, Form } from "formik";
 // import { getMe, deleteAlbum } from '../utils/API';
 // import Auth from '../utils/auth';
 // import loading from '../loading.gif'
@@ -13,7 +14,22 @@ function MyAlbums() {
   return (
     <div  >
     <Container > 
-          {/*addition of formik  */}
+    <div className="App">
+      <h1>Contact Us</h1>
+      <Formik
+        initialValues={{ breed: "", petType: "" }}
+        onSubmit={async (values) => {
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <Field name="name" type="text" />
+          <Field name="email" type="email" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
       </Container>
     </div>
   );
