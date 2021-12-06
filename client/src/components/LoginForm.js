516ee01397889e0b9158a2566dcffa715e1be25c
Matt Weichel <maweiche@gmail.com>
2021-12-06 11:16:37 -0800

    fixed login issue

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -26,10 +26,15 @@ const LoginForm = () => {
         }
     
         try {
-          const { data } = await loginUser({
-            variables: {...userFormData}
-          });
-          Auth.login(data.login.token);
+          const response = await loginUser(userFormData);
+
+          if(!response.ok) {
+            throw new Error('oops! try again!');
+          }
+          
+          const { token, user } = await response.json();
+          console.log(user);
+          Auth.login(token);
         } catch (err) {
           console.error(err);
           setShowAlert(true);
